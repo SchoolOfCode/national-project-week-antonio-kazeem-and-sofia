165f1b31d47d86e73bc18893e0e881d3e8e1163a
Sevi <correosevi@gmail.com>
2022-01-17 20:05:22 +0000

    Clean unused files from install, close #4

---
+++ client/src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="login-container">Login Screen</div>
+    </div>
+  );
+}
+
+export default App;
