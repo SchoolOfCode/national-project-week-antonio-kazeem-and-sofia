92024fbc288cb1660581fd9ab4fdfb67c51c899f
Sevi <correosevi@gmail.com>
2022-01-18 11:40:06 +0000

    Button & Table components created

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Contacts from '../Contacts/index';
+import Table from '../../components/Table/index';
 
 function App() {
   return (
     <div className="App">
       {/* <div className="login-container">Login Screen</div> */}
-      <Contacts />
+      <Table />
     </div>
   );
 }
