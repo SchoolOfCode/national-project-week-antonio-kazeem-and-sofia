8bdf27f843295982c2be1a5398e55b60084ffe7d
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-18 12:13:43 +0000

    Database connection done

--- server/models/users.js
+++ server/models/users.js
@@ -1,11 +1,19 @@
 import query from '../db/index.js';
 
 export const createUsers = async (body) => {
-    const { fname, lname, email, githubUser, password, location, interests } =
+
+    const { fname, lname, email, githubUser, password, location, intrests } =
         body;
     const response = await query(
-        'INSERT INTO users(f_name, l_name, email, githubUser, password, location, interests) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-        [fname, lname, email, githubUser, password, location, interests]
+        'INSERT INTO users(f_name, l_name, email, githubUser, password, location, intrests) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+        [fname, lname, email, githubUser, password, location, intrests]
     );
-    return response;
+    return response.rows;
+};
+export const getUsers = async () => {
+    
+    const response = await query(
+       "SELECT * FROM users"
+    );
+    return response.rows;
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,19 @@
-import express from 'express';
-import { createUsers } from '../models/users.js';
+import express from "express";
+import { createUsers, getUsers } from "../models/users.js";
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.json({ message: 'I wish we had some information to give you ☹️' });
+router.get("/", async function(req, res) {
+  const getUser = await getUsers();
+  res.json({ success: true, message: "Its working", payload: getUser });
 });
 
-router.post('/', async (req, res) => {
-    const createUser = await createUsers(req.body);
-    res.json({
-        success: true,
-        message: 'User created...',
-        payload: createUser,
-    });
+router.post("/", async (req, res) => {
+  const createUser = await createUsers(req.body);
+  res.json({
+    success: true,
+    message: "User created...",
+    payload: createUser
+  });
 });
 export default router;
