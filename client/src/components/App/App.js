e686791d880da1b8eb593c186157f21131061570
Sevi <correosevi@gmail.com>
2022-01-18 11:16:17 +0000

    Main table desing finished

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Contacts from '../Contacts/index';
 
 function App() {
   return (
     <div className="App">
-      <div className="login-container">Login Screen</div>
+      {/* <div className="login-container">Login Screen</div> */}
+      <Contacts />
     </div>
   );
 }
