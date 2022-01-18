eb8718dea2216dad0c78dc7e00c5a516eebb5949
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-18 14:14:38 +0000

    React started

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -6,14 +6,14 @@ function Login() {
       <label>Email</label>
       <br></br>
       <input />
-      <br></br>
-      <br></br>
+      <br />
+      <br />
 
       <label>Password</label>
       <br></br>
       <input />
-      <br></br>
-      <br></br>
+      <br />
+      <br />
     </div>
   );
 }
