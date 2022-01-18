94a16b53cb9668cdeee8f2f084b2bc51c52db4a8
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-18 13:36:20 +0000

    Routes for login and Register created

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-
+import "./App.css";
+import Welcome from "../Welcome";
+import { Route, Routes } from "react-router-dom";
+import Login from "../Login";
+import Register from "../Register";
 function App() {
   return (
     <div className="App">
-      <div className="login-container">Login Screen</div>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Login/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <label>Email</label>
+      <br></br>
+      <input />
+      <br></br>
+      <br></br>
+
+      <label>Password</label>
+      <br></br>
+      <input />
+      <br></br>
+      <br></br>
+    </div>
+  );
+}
+
+export default Login;
