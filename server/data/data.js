19d6d526b9e64998c4272538d61a3b9663e8390f
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-19 13:29:03 +0000

    HEroku started

--- server/data/data.js
+++ server/data/data.js
@@ -43,142 +43,142 @@ export const data = [
     password: "password5",
     location: "Someret",
     intrests: "Hiking"
+  },
+  {
+      fname: "Isobella",
+      lname: "Lopez",
+      email: "izzy.l@outlook.co.uk",
+      githubUser: "izzyl9",
+      password: "password6",
+      location: "London",
+    intrests: "Music"
+  },
+  {
+      "fname": "Samuel",
+      "lname": "Dolan",
+      "email": "dolans3@aol.com",
+      "githubUser": "kingcoder2",
+      "password": "password7",
+      "location": "Hull",
+      "intrests": "Biking"
+  },
+  {
+      "fname": "Penelope",
+      "lname": "Clearwater",
+      "email": "pennyc@gmail.com",
+      "githubUser": "cpenny00",
+      "password": "password8",
+      "location": "Southampton",
+      "intrests": "Hiking"
+  },
+  {
+      fname: "Anna",
+      lname: "Hooper",
+      email: "a.hooper@hotmail.com",
+      githubUser: "littleanna3",
+      password: "password9",
+      location: "Barnsley",
+      intrests: "Shopping"
+  },
+  {
+      fname: "Attia",
+      lname: "Ali",
+      email: "ali.a@aol.com",
+      githubUser:"atiasworld7",
+      password: "password10",
+      location:"Hereford",
+      intrests: "Astronomy"
+  },
+  {
+      fname: "Trang",
+      lname: "Nguyen",
+      email: "Trang11n@outlook.co.uk",
+      githubUser: "happy299t",
+      password: "password11",
+      location: "Bristol",
+      intrests: "Astology"
+  },
+  {
+      fname: "Paisley",
+      lname: "Bako",
+      email: "bakopaisley@gmail.co.uk",
+      githubUser: "bakoisbest2",
+      password: "password12",
+      location: "Coventry",
+      intrests: "Walking"
+  },
+  {
+      fname: "Louis",
+      lname: "Gonzales",
+      email: "louis.g77@outlook.com",
+      githubUser: "gonzy77l",
+      password: "password13",
+      location: "Birmingham",
+      intrests: "Reading"
+  },
+  {
+      "fname": "Hayley",
+      "lname": "Smith",
+      "email": "haysmith@aol.com",
+      "githubUser": "hayhay88",
+      "password": "password14",
+      "location": "Hull",
+      "intrests": "Biking"
+  },
+  {
+    "fnam": "karam",
+    "lnam": "Kaur",
+    "emai": "kkaur@hotmail.co.uk",
+  "githubUse": "kaurkamkam",
+    "passwor": "passwor15",
+    "locatio": "London",
+    "intrest": "Astronomy"
+  },
+  {
+      fname: "Angelo",
+      lname: "Da Silva",
+      email: "angelo.d.silva@hotmail.com",
+      githubUser: "a.da.silva33",
+      password: "password16",
+      location: "Manchester",
+      intrests: "Biking"
+  },
+  {
+      fname: "Emily",
+      lname: "Harris",
+      email: "emilyharris@gmail.com",
+      githubUser: "emem94",
+      password: "password17",
+      location: "Surrey",
+      intrests: "Gym"
+  },
+  {
+      fname: "Antonio",
+      lname: "Sevillano",
+      email: "test@test.com",
+      githubUser: "ajsevillano",
+      password: "password18",
+      location: "Somewhere",
+      intrests: "Astronomy"
+  },
+  {
+      fname: "yan",
+      lname: "Thompson",
+      email: "ry22thompson@aol.com",
+      githubUser:"ryryt22",
+      password: "password19",
+      location: "Brighton",
+      intrests: "Biking"
+  },
+  {
+      fname: "Jemima",
+      lname: "O’hara",
+      email: "j.ohara@gmail.com",
+      githubUser: "jemimao",
+      password: "password20",
+      location: "jjohara88",
+      intrests: "Reading"
   }
-  // {
-  //     “fname”: “Isobella”,
-  //     “lname”: “Lopez”,
-  //     “email”: “izzy.l@outlook.co.uk”,
-  //     “githubUser”: “izzyl9",
-  //     “password”: “password6",
-  //     “location”: “London”,
-  //     “intrests”: “Music”
-  // }
-  // {
-  //     “fname”: “Samuel”,
-  //     “lname”: “Dolan”,
-  //     “email”: “dolans3@aol.com”,
-  //     “githubUser”: “kingcoder2",
-  //     “password”: “password7",
-  //     “location”: “Hull”,
-  //     “intrests”: “Biking”
-  // }
-  // {
-  //     “fname”: “Penelope”,
-  //     “lname”: “Clearwater”,
-  //     “email”: “pennyc@gmail.com”,
-  //     “githubUser”: “cpenny00",
-  //     “password”: “password8",
-  //     “location”: “Southampton”,
-  //     “intrests”: “Hiking”
-  // }
-  // {
-  //     “fname”: “Anna”,
-  //     “lname”: “Hooper”,
-  //     “email”: “a.hooper@hotmail.com”,
-  //     “githubUser”: “littleanna3",
-  //     “password”: “password9",
-  //     “location”: “Barnsley”,
-  //     “intrests”: “Shopping”
-  // }
-  // {
-  //     “fname”: “Attia”,
-  //     “lname”: “Ali”,
-  //     “email”: “ali.a@aol.com”,
-  //     “githubUser”: “atiasworld7",
-  //     “password”: “password10",
-  //     “location”: “Hereford”,
-  //     “intrests”: “Astronomy”
-  // }
-  // {
-  //     “fname”: “Trang”,
-  //     “lname”: “Nguyen”,
-  //     “email”: “Trang11n@outlook.co.uk”,
-  //     “githubUser”: “happy299t”,
-  //     “password”: “password11",
-  //     “location”: “Bristol”,
-  //     “intrests”: “Astology”
-  // }
-  // {
-  //     “fname”: “Paisley”,
-  //     “lname”: “Bako”,
-  //     “email”: “bakopaisley@gmail.co.uk”,
-  //     “githubUser”: “bakoisbest2",
-  //     “password”: “password12",
-  //     “location”: “Coventry”,
-  //     “intrests”: “Walking”
-  // }
-  // {
-  //     “fname”: “Louis”,
-  //     “lname”: “Gonzales”,
-  //     “email”: “louis.g77@outlook.com”,
-  //     “githubUser”: “gonzy77l”,
-  //     “password”: “password13",
-  //     “location”: “Birmingham”,
-  //     “intrests”: “Reading”
-  // }
-  // {
-  //     “fname”: “Hayley”,
-  //     “lname”: “Smith”,
-  //     “email”: “haysmith@aol.com”,
-  //     “githubUser”: “hayhay88",
-  //     “password”: “password14",
-  //     “location”: “Hull”,
-  //     “intrests”: “Biking”
-  // }
-  // {
-  //     “fname”: “karam”,
-  //     “lname”: “Kaur”,
-  //     “email”: “kkaur@hotmail.co.uk”,
-  //     “githubUser”: “kaurkamkam”,
-  //     “password”: “passwor15",
-  //     “location”: “London”,
-  //     “intrests”: “Astronomy”
-  // }
-  // {
-  //     “fname”: “Angelo”,
-  //     “lname”: “Da Silva”,
-  //     “email”: “angelo.d.silva@hotmail.com”,
-  //     “githubUser”: “a.da.silva33",
-  //     “password”: “password16",
-  //     “location”: “Manchester”,
-  //     “intrests”: “Biking”
-  // }
-  // {
-  //     “fname”: “Emily”,
-  //     “lname”: “Harris”,
-  //     “email”: “emilyharris@gmail.com”,
-  //     “githubUser”: “emem94",
-  //     “password”: “password17",
-  //     “location”: “Surrey”,
-  //     “intrests”: “Gym”
-  // }
-  // {
-  //     “fname”: “Antonio”,
-  //     “lname”: “Sevillano”,
-  //     “email”: “test@test.com”,
-  //     “githubUser”: “ajsevillano”,
-  //     “password”: “password18",
-  //     “location”: “Somewhere”,
-  //     “intrests”: “Astronomy”
-  // }
-  // {
-  //     “fname”: “Ryan”,
-  //     “lname”: “Thompson”,
-  //     “email”: “ry22thompson@aol.com”,
-  //     “githubUser”: “ryryt22",
-  //     “password”: “password19",
-  //     “location”: “Brighton”,
-  //     “intrests”: “Biking”
-  // }
-  // {
-  //     “fname”: “Jemima”,
-  //     “lname”: “O’hara”,
-  //     “email”: “j.ohara@gmail.com”,
-  //     “githubUser”: “jemimao”,
-  //     “password”: “password20",
-  //     “location”: “jjohara88",
-  //     “intrests”: “Reading”
-  // }
   // {
   //     “fname”: “Gurdeep”,
   //     “lname”: “Manalia”,
