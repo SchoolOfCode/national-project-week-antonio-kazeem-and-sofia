3ef66861ed05f172490e539d685920f263f87cf6
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-19 20:17:57 +0000

    New db credentials added

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -4,7 +4,8 @@ import Header from "./Header";
 import Row from "./Row";
 import "./table.css";
 
-const API_URL = process.env.REACT_APP_API_
+// const API_URL = process.env.REACT_APP_API_
+const API_URL = "http://localhost:3001";
 
 const Contacts = () => {
   const [userData, setuserData] = useState([]);
