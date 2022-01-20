27d31d17a8f2d9d3e45cb5af551f7b358662103b
Sevi <correosevi@gmail.com>
2022-01-20 14:37:01 +0000

    Fix Journal import

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -6,9 +6,7 @@ import Home from "../Home/index.js";
 import OnetoOne from "../1to1";
 import linksDocs from "../LinksDocs/index.js";
 import Journal from "../Journal";
-import Journal from "../Journal";
 import Register from "../Register";
-
 
 function App() {
   return (
@@ -23,7 +21,6 @@ function App() {
           <Route path="/Journal" element={<Journal />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/Links&Docs" element={<linksDocs />} />
-
         </Routes>
       </div>
     </div>
