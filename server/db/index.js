a71d1232a90c2436e2623f5815074173d9bdba0c
Sevi <correosevi@gmail.com>
2022-01-20 12:38:42 +0000

    Delete modules

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,16 @@
-import pg from 'pg';
-// import { db } from '../config.js';
-
+import pg from "pg";
+import { db } from "../config.js";
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  // PRODUCTION  connectionString: process.env.DATABASE_URL,
+  user: db.user,
+  host: db.host,
+  database: db.database,
+  password: db.password,
+  port: db.port,
   ssl: { rejectUnauthorized: false }
 });
 
 export default async function query(text, params) {
-    return pool.query(text, params);
+  return pool.query(text, params);
 }
