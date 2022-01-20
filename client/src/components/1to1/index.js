e5fbaabeffc8f2a1de31b30fe2364db6098adb50
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-20 11:26:32 +0000

    Table created for things to do

--- client/src/components/1to1/index.js
+++ client/src/components/1to1/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React from "react";
+import "./1to1.css";
 
 function OnetoOne() {
-  return <div></div>;
+  // const date = Date.now().toString()
+
+  return (
+    <div>
+      <h1 id="header">Welcome to your 1to1 Section</h1>
+      <br></br>
+      <div className="meeting-details">
+        <p>Coach:Abdullahi</p>
+        <p>Your next meeting is on 23/11/2021 </p>
+        <p>Meeting Zoom link: </p>
+      </div>
+
+      <div className="input-field">
+        Things to work on before the next meeting
+        <input></input>
+      </div>
+      <div className="table">
+        <table>
+          <tr>
+            <th>ggc</th>
+            <th>ggg</th>
+            <th>gg</th>
+          </tr>
+          <tr>
+            <td>ggg</td>
+            <td>ggf</td>
+            <td>gg</td>
+          </tr>
+          <tr>
+            <td>ggff</td>
+            <td>ggff</td>
+            <td>ggffff</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default OnetoOne;
