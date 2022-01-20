d6d3ba465241a2479f70f49bd350026f0cad4a55
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-20 15:12:12 +0000

    1to1 created

--- client/src/components/1to1/index.js
+++ client/src/components/1to1/index.js
@@ -1,30 +1,62 @@
-import React from "react";
-import "./1to1.css";
 import { useState } from "react";
-import Input from "../Input1to1";
+import { v4 as uuidv4 } from "uuid";
+import InputJournal from "../InputJournal/";
+import SelectJournal from "../SelectJournal";
+import ButtonJournal from "../ButtonJournal";
+import List from "../List";
+import "./1to1.css";
 
-function OnetoOne() {
-  const [input, setInput] = useState("");
-  const [inputList, setInputList] = useState([]);
+const Journal = () => {
+  const [listTodo, setListToDo] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("Low");
+
+  function handleInput(e) {
+    setInputValue(e.target.value);
+  }
 
   function handleButton() {
-    setInputList([...inputList, { input }]);
+    setListToDo([
+      ...listTodo,
+      {
+        id: uuidv4(),
+        task: inputValue,
+        priority: selectValue,
+        assigned: "name"
+      }
+    ]);
+    reset();
+  }
+
+  function reset() {
+    setInputValue("");
+  }
+
+  function handleSelect(e) {
+    setSelectValue(e.target.value);
   }
 
   return (
     <div>
-      <h1 id="header">Welcome to your 1to1 Section</h1>
-      <br></br>
-      <div className="meeting-details">
-        <p>Coach:Abdullahi</p>
-        <p>Your next meeting is on 23/11/2021 </p>
-        <p>Meeting Zoom link: </p>
-      </div>
       <div>
-        <Input />
+        <h1 className="section">Your 1-1 Section</h1>
+        <div className="meeting-details">
+          <p className="coach">Coach: Abdullah</p>
+          <p className="coach">Your next meeting is on 23/11/2021</p>
+          <p className="coach">Zoom link:</p>
+        </div>
+      </div>
+      <div className="section">
+        <p className="things section">Things to do before the next meeting</p>
+        <div className="list-container">
+          <InputJournal handleInput={handleInput} inputValue={inputValue} />
+          <SelectJournal handleSelect={handleSelect} />
+          <ButtonJournal handleButton={handleButton} />
+        </div>
+        <List listTodo={listTodo} setListToDo={setListToDo} />
       </div>
     </div>
   );
-}
+};
 
-export default OnetoOne;
+export default Journal;
