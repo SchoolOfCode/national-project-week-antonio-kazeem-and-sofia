675ba25ab784b87054269bed15c0626d972750eb
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-20 16:58:37 +0000

    Todo path deleted

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import logger from "morgan";
 
 import usersRouter from "./routes/users.js";
-import todoRouter from "./routes/todos.js"
+// import todoRouter from "./routes/todos.js"
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", usersRouter);
-app.use("/todos", todoRouter);
+// app.use("/todos", todoRouter);
 
 // app.use(function (req, res, next) {
 //   res.status(404).json({message: "We couldn't find what you were looking for 😞"})
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,20 +1,21 @@
-import express from "express";
-import { createtodo, getTodos } from "../models/users.js";
-const router = express.Router();
+// import express from "express";
+// import { createtodo, getTodos } from "../models/todos.js";
+// const router = express.Router();
 
-/* GET users listing. */
-router.get("/", async function (req, res) {
-  const getTodoList = await getTodos();
-  res.json({ success: true, message: "Its working", payload: getTodoList });
-});
+// /* GET users listing. */
+// router.get("/", async function (req, res) {
+//   // const getTodoList = await getTodos();
+//   // res.json({ success: true, message: "Its working", payload: getTodoList });
+//   res.json({message:"its working"})
+// });
 
-router.post("/", async (req, res) => {
-  const createTodos = await createtodo(req.body);
-  res.json({
-    success: true,
-    message: "User created...",
-    payload: createTodos
-  });
-});
+// router.post("/", async (req, res) => {
+//   const createTodos = await createtodo(req.body);
+//   res.json({
+//     success: true,
+//     message: "User created...",
+//     payload: createTodos
+//   });
+// });
 
-export default router;
+// export default router;
